0a749610ed55b34c10dafb9ce7dbf16fa4b41b7a
Aldin Drobic <aldr0005@student.hv.se>
2020-11-17 14:56:22 +0100

    Ändrat till min branch

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-        <Text>Hej</Text>
+        <Text>Ändrar min branch</Text>
     </View>
   );
 }
